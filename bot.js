d9b03ecc768b41dac1578727cb5a20b18ea00c3b
mattnguyen1 <mattnguyen1@yahoo.com>
2015-12-15 22:09:38 -0500

    fixed heroku bug

--- bot.js
+++ bot.js
@@ -11,6 +11,20 @@ var request = require('request');
 var bot = new Discord.Client();
 var name = "";
 
+// FIX STUPID BUG
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 var commands = {
 	"gif" : {
 		"protocol" : function(suffix, callback) {
