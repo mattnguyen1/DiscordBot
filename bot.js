231361f0c54fc4587c0c8410559146f36f388bac
mattnguyen1 <mattnguyen1@yahoo.com>
2016-01-11 13:31:15 -0500

    took out helpers dependency

--- bot.js
+++ bot.js
@@ -143,14 +143,14 @@ var slash_commands = {
 		"protocol" : function(message, callback) {
 			var sfx_arr = message.content.split(' ');
 			if (sfx_arr.length == 1) {
-				callback(null,helpers.roll(message.author, 1, 100));
+				callback(null,roll(message.author, 1, 100));
 			}
 			if (sfx_arr.length == 2) {
-				if (helpers.isPositiveInteger(sfx_arr[1])) {
-					callback(null,helpers.roll(message.author, sfx_arr[1]));
+				if (isPositiveInteger(sfx_arr[1])) {
+					callback(null,roll(message.author, sfx_arr[1]));
 				}
 			} else if (sfx_arr.length == 3) {
-				callback(null,helpers.roll(message.author, sfx_arr[1], sfx_arr[2]));
+				callback(null,roll(message.author, sfx_arr[1], sfx_arr[2]));
 			}
 		}
 	}
@@ -302,6 +302,28 @@ function google(query, callback) {
 			}
 		}
 	}.bind(this));
+}
+
+function isPositiveInteger(n) {
+    return 0 === n % (!isNaN(parseFloat(n)) && 0 <= ~~n);
+}
+
+function roll(user, a, b) {
+	if (b === undefined || b === null) {
+		var rand_roll = Math.floor(Math.random()*a);
+		if (a > 0) {
+			return user + " rolls " + rand_roll + " (1-" + a + ")";
+		} else {
+			return user + " rolls " + rand_roll + " (" + a + "-0)";
+		}
+	// 2 numbers given, roll from a to b, assuming a < b
+	} else {
+		if (a > b) {
+			return "Rolls require the first number to be less than the second.";
+		}
+		var rand_roll = Math.floor(Math.random()*(b-a)+a);
+		return user + " rolls " + rand_roll + " (" + a + "-" + b + ")"; 
+	}
 }
 
 // Bot initializiation
