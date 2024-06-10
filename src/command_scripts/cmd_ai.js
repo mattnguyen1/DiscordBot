f8abd5700ac25624ca0a0168fa8f9d37a5fc636d
Matt Nguyen <mattnguyen94@gmail.com>
2024-06-10 16:44:44 -0700

    add ai

---
+++ src/command_scripts/cmd_ai.js
@@ -0,0 +1,46 @@
+/**
+ * @file cmd_img
+ * @author mattnguyen1
+ */
+
+// ---------------------------------
+// Requirements
+// ---------------------------------
+
+const OpenAi = require("openai");
+const conf = require("../config");
+const request = require("request");
+
+// ---------------------------------
+// Private
+// ---------------------------------
+
+const openai = new OpenAi();
+
+/**
+ * Generates an open ai image
+ * @param  {Object} options
+ * @param  {Message} message
+ * @param  {Function} callback
+ * @return {void}
+ */
+let _genImage = async (options, message, callback) => {
+  try {
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: message.content,
+      n: 1,
+      size: "1024x1024",
+    });
+    const image_url = response.data[0].url;
+    callback(image_url);
+  } catch (e) {
+    callback("Error generating image.");
+  }
+};
+
+module.exports.ai = {
+  run: _genImage,
+  usage: "ai <query>",
+  description: "Returns an ai generated image based on query.",
+};
